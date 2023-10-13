fdbcc6f88d512d9c33ac283b6f9acc664cd959ba
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-10-13 23:45:12 +0200

    PageMaturaYears content

--- PageMaturaYears.xaml.cs
+++ PageMaturaYears.xaml.cs
@@ -24,5 +24,51 @@ namespace Abituria
         {
             InitializeComponent();
         }
+        private void ButtonAbituria(object sender, RoutedEventArgs e)
+        {
+            var mainWin = new MainWindow();
+            mainWin.Show();
+
+        }
+        private void ButtonKalkulator(object sender, RoutedEventArgs e)
+        {
+            var calculator = new Calculator();
+            calculator.Show();
+        }
+        private void ButtonMatura(object sender, RoutedEventArgs e)
+        {
+            PageMatura pageMatura = new PageMatura();
+            NavigationService.Navigate(pageMatura);
+        }
+        private void ButtonDzialy(object sender, RoutedEventArgs e)
+        {
+            PageChapters pageChapters = new PageChapters();
+            NavigationService.Navigate(pageChapters);
+        }
+        private void ButtonZadania(object sender, RoutedEventArgs e)
+        {
+            PageExercises pageExercises = new PageExercises();
+            NavigationService.Navigate(pageExercises);
+        }
+        private void ButtonWideo(object sender, RoutedEventArgs e)
+        {
+            PageVideo pageVideo = new PageVideo();
+            NavigationService.Navigate(pageVideo);
+        }
+        private void Button2021(object sender, RoutedEventArgs e)
+        {
+            PageMatura2021 pageMatura2021 = new PageMatura2021();
+            NavigationService.Navigate(pageMatura2021);
+        }
+        private void Button2020(object sender, RoutedEventArgs e)
+        {
+            PageMatura2020 pageMatura2020 = new PageMatura2020();
+            NavigationService.Navigate(pageMatura2020);
+        }
+        private void Button2019(object sender, RoutedEventArgs e)
+        {
+            PageMatura2019 pageMatura2019 = new PageMatura2019();
+            NavigationService.Navigate(pageMatura2019);
+        }
     }
 }
