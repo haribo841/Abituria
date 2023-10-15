3a358fd3abde28f29dc45c79ff7f98fe78d3bc01
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-10-15 20:25:21 +0200

    Chapters

---
+++ PageMaturaCorrection2021.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abituria
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PageMaturaCorrection2021.xaml
+    /// </summary>
+    public partial class PageMaturaCorrection2021 : Page
+    {
+        public PageMaturaCorrection2021()
+        {
+            InitializeComponent();
+        }
+    }
+}
