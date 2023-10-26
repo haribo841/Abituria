56259f49837a4e069067947e094ce8a166b29288
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-10-26 13:33:31 +0200

    Folders structure

---
+++ Menu/Calculators/CalcQuadraticFunc.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abituria.Menu.Calculators
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CalcQuadraticFunc.xaml
+    /// </summary>
+    public partial class CalcQuadraticFunc : Page
+    {
+        public CalcQuadraticFunc()
+        {
+            InitializeComponent();
+        }
+    }
+}
