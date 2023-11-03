39bdef461ba960a1c2745b98de2e5198996edb68
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-11-03 11:49:59 +0100

    MP21 content

--- Menu/Calculators/CalcQuadraticFunc.xaml.cs
+++ Menu/Calculators/CalcQuadraticFunc.xaml.cs
@@ -30,11 +30,17 @@ namespace Abituria.Menu.Calculators
             {
                 case Visibility.Visible:
                     {
-                        float warA = float.Parse(fieldA.Text);
-                        StandardForm(warA, 20, 10);
+                        double a = double.Parse(fieldA.Text);
+                        double b = double.Parse(fieldB.Text);
+                        double c = double.Parse(fieldC.Text);
+                        StandardForm(a, b, c);
                         break;
                     }
 
+                case Visibility.Hidden:
+                    break;
+                case Visibility.Collapsed:
+                    break;
                 default:
                     MessageBox.Show("WIP");
                     break;
@@ -45,26 +51,56 @@ namespace Abituria.Menu.Calculators
         {
             MessageBox.Show("WIP");
         }
-        private void StandardForm(float a, float b, float c)
+        private void StandardForm(double a, double b, double c)
         {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            double x0 = -b / (2 * a);
+            double x1 = Math.Round((-b - Math.Sqrt(delta)) / (2 * a), 2);
+            double x2 = Math.Round((-b + Math.Sqrt(delta)) / (2 * a), 2);
 
-            float result = a + b + c;
 
-            MessageBox.Show("WIP " + result);
+            switch (delta)
+            {
+                case < 0:
+                    {
+                        string resultTxt = "Δ < 0, funkcja nie posiada miejsc zerowych";
+                        result.Text = resultTxt;
+                        break;
+                    }
+
+                case 0:
+                    {
+                        string resultTxt = "Δ = 0, funkcja posiada jedno miejsce zerowe, gdzie wierzchołek dotyka osi x";
+                        result.Text = resultTxt;
+                        break;
+                    }
+
+                default:
+                    {
+                        string resultTxt = $"Δ > 0, funkcja posiada dwa miejsca zerowe: x1 = {x1} i x2 = {x2}";
+                        result.Text = resultTxt;
+                        break;
+                    }
+            }
+
+            this.resultHead.Visibility = Visibility.Visible;
+            this.result.Visibility = Visibility.Visible;
         }
         private void ButtonStandard(object sender, RoutedEventArgs e)
         {
-            switch (pOgolna.Visibility)
+            switch (StandardGroup.Visibility)
             {
-                case Visibility.Collapsed when buttonPrzelicz.Visibility == Visibility.Collapsed && buttonReset.Visibility == Visibility.Collapsed:
-                    pOgolna.Visibility = Visibility.Visible;
-                    buttonPrzelicz.Visibility = Visibility.Visible;
+                case Visibility.Collapsed when buttonCalculate.Visibility == Visibility.Collapsed && buttonReset.Visibility == Visibility.Collapsed:
+                    StandardGroup.Visibility = Visibility.Visible;
+                    buttonCalculate.Visibility = Visibility.Visible;
                     buttonReset.Visibility = Visibility.Visible;
                     break;
                 default:
-                    pOgolna.Visibility = Visibility.Collapsed;
-                    buttonPrzelicz.Visibility = Visibility.Collapsed;
+                    StandardGroup.Visibility = Visibility.Collapsed;
+                    buttonCalculate.Visibility = Visibility.Collapsed;
                     buttonReset.Visibility = Visibility.Collapsed;
+                    this.resultHead.Visibility = Visibility.Collapsed;
+                    this.result.Visibility = Visibility.Collapsed;
                     break;
             }
         }
