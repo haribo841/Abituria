b240c21144ee04125908a6330726369bce2409ca
haribo841 <adamwaldemarkubis@gmail.com>
2023-11-04 21:48:36 +0100

    Nav prototype

--- Menu/Matura/2021p/PageMP21Z1.xaml.cs
+++ Menu/Matura/2021p/PageMP21Z1.xaml.cs
@@ -31,7 +31,7 @@ namespace Abituria
             mainWin.Show();
 
         }
-        private void ButtonKalkulator(object sender, RoutedEventArgs e)
+        private void ButtonCalculator(object sender, RoutedEventArgs e)
         {
             var calculator = new Calculator();
             calculator.Show();
@@ -41,17 +41,17 @@ namespace Abituria
             PageMaturaYears pageMaturaYears = new PageMaturaYears();
             NavigationService.Navigate(pageMaturaYears);
         }
-        private void ButtonDzialy(object sender, RoutedEventArgs e)
+        private void ButtonChapters(object sender, RoutedEventArgs e)
         {
             PageChapters pageChapters = new PageChapters();
             NavigationService.Navigate(pageChapters);
         }
-        private void ButtonZadania(object sender, RoutedEventArgs e)
+        private void ButtonExercises(object sender, RoutedEventArgs e)
         {
             PageExercises pageExercises = new PageExercises();
             NavigationService.Navigate(pageExercises);
         }
-        private void ButtonWideo(object sender, RoutedEventArgs e)
+        private void ButtonVideo(object sender, RoutedEventArgs e)
         {
             PageVideo pageVideo = new PageVideo();
             NavigationService.Navigate(pageVideo);
@@ -70,6 +70,175 @@ namespace Abituria
         {
             PageMP21Z3 pageMP21Z3 = new PageMP21Z3();
             NavigationService.Navigate(pageMP21Z3);
+        }
+        private void MP2021Exe4(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z4 pageMP21Z4 = new PageMP21Z4();
+            NavigationService.Navigate(pageMP21Z4);
+        }
+        private void MP2021Exe5(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z5 pageMP21Z5 = new PageMP21Z5();
+            NavigationService.Navigate(pageMP21Z5);
+        }
+        private void MP2021Exe6(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z6 pageMP21Z6 = new PageMP21Z6();
+            NavigationService.Navigate(pageMP21Z6);
+        }
+        private void MP2021Exe7(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z7 pageMP21Z7 = new PageMP21Z7();
+            NavigationService.Navigate(pageMP21Z7);
+        }
+        private void MP2021Exe8(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z8 pageMP21Z8 = new PageMP21Z8();
+            NavigationService.Navigate(pageMP21Z8);
+        }
+        private void MP2021Exe9(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z9 pageMP21Z9 = new PageMP21Z9();
+            NavigationService.Navigate(pageMP21Z9);
+        }
+        private void MP2021Exe10(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z10 pageMP21Z10 = new PageMP21Z10();
+            NavigationService.Navigate(pageMP21Z10);
+        }
+        private void MP2021Exe11(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z11 pageMP21Z11 = new PageMP21Z11();
+            NavigationService.Navigate(pageMP21Z11);
+        }
+        private void MP2021Exe12(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z12 pageMP21Z12 = new PageMP21Z12();
+            NavigationService.Navigate(pageMP21Z12);
+        }
+        private void MP2021Exe13(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z13 pageMP21Z13 = new PageMP21Z13();
+            NavigationService.Navigate(pageMP21Z13);
+        }
+        private void MP2021Exe14(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z14 pageMP21Z14 = new PageMP21Z14();
+            NavigationService.Navigate(pageMP21Z14);
+        }
+        private void MP2021Exe15(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z15 pageMP21Z15 = new PageMP21Z15();
+            NavigationService.Navigate(pageMP21Z15);
+        }
+        private void MP2021Exe16(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z16 pageMP21Z16 = new PageMP21Z16();
+            NavigationService.Navigate(pageMP21Z16);
+        }
+        private void MP2021Exe17(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z17 pageMP21Z17 = new PageMP21Z17();
+            NavigationService.Navigate(pageMP21Z17);
+        }
+        private void MP2021Exe18(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z18 pageMP21Z18 = new PageMP21Z18();
+            NavigationService.Navigate(pageMP21Z18);
+        }
+        private void MP2021Exe19(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z19 pageMP21Z19 = new PageMP21Z19();
+            NavigationService.Navigate(pageMP21Z19);
+        }
+        private void MP2021Exe20(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z20 pageMP21Z20 = new PageMP21Z20();
+            NavigationService.Navigate(pageMP21Z20);
+        }
+        private void MP2021Exe21(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z21 pageMP21Z21 = new PageMP21Z21();
+            NavigationService.Navigate(pageMP21Z21);
+        }
+        private void MP2021Exe22(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z22 pageMP21Z22 = new PageMP21Z22();
+            NavigationService.Navigate(pageMP21Z22);
+        }
+        private void MP2021Exe23(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z23 pageMP21Z23 = new PageMP21Z23();
+            NavigationService.Navigate(pageMP21Z23);
+        }
+        private void MP2021Exe24(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z24 pageMP21Z24 = new PageMP21Z24();
+            NavigationService.Navigate(pageMP21Z24);
+        }
+        private void MP2021Exe25(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z25 pageMP21Z25 = new PageMP21Z25();
+            NavigationService.Navigate(pageMP21Z25);
+        }
+        private void MP2021Exe26(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z26 pageMP21Z26 = new PageMP21Z26();
+            NavigationService.Navigate(pageMP21Z26);
+        }
+        private void MP2021Exe27(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z27 pageMP21Z27 = new PageMP21Z27();
+            NavigationService.Navigate(pageMP21Z27);
+        }
+        private void MP2021Exe28(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z28 pageMP21Z28 = new PageMP21Z28();
+            NavigationService.Navigate(pageMP21Z28);
+        }
+        private void MP2021Exe29(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z29 pageMP21Z29 = new PageMP21Z29();
+            NavigationService.Navigate(pageMP21Z29);
+        }
+        private void MP2021Exe30(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z30 pageMP21Z30 = new PageMP21Z30();
+            NavigationService.Navigate(pageMP21Z30);
+        }
+        private void MP2021Exe31(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z31 pageMP21Z31 = new PageMP21Z31();
+            NavigationService.Navigate(pageMP21Z31);
+        }
+        private void MP2021Exe32(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z32 pageMP21Z32 = new PageMP21Z32();
+            NavigationService.Navigate(pageMP21Z32);
+        }
+        private void MP2021Exe33(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z33 pageMP21Z33 = new PageMP21Z33();
+            NavigationService.Navigate(pageMP21Z33);
+        }
+        private void MP2021Exe34(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z34 pageMP21Z34 = new PageMP21Z34();
+            NavigationService.Navigate(pageMP21Z34);
+        }
+        private void MP2021Exe35(object sender, RoutedEventArgs e)
+        {
+            PageMP21Z35 pageMP21Z35 = new PageMP21Z35();
+            NavigationService.Navigate(pageMP21Z35);
+        }
+        /*private void ConfirmBtn(object sender, RoutedEventArgs e)
+        {
+            bool ansChecked = CheckAnswer(correctAnsw);
+            return isAnsCorrect;
+        }*/
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
