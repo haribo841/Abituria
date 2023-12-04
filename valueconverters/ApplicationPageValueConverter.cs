f50bc1eafdf4621b91850e87c5d8cab4697e675a
Adam <adamwaldemarkubis@gmail.com>
2023-12-04 21:08:42 +0100

    Attached properites

--- valueconverters/ApplicationPageValueConverter.cs
+++ valueconverters/ApplicationPageValueConverter.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Abituria.pages;
+using System.IO;
+using Abituria.datamodels;
 
 namespace Abituria.valueconverters
 {
-    class ApplicationPageValueConverter
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>///Zamienia strone aplikacji na aktualną strone
     {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((ApplicationPage)value)///Znajdź odpowiednią strone
+            {
+                case ApplicationPage.Login:
+                    return new LoginPage();
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- viewmodel/base/BaseViewModel.cs
+++ viewmodel/base/BaseViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Abituria.viewmodel.base
+namespace Abituria.viewmodel
 {
-    class BaseViewModel
+    [ImplementPropertyChanged]
+    public class BaseViewModel : INotifyPropertyChanged///Bazowy model widoku odpala Property Changed kiedy trzeba
     {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) =>///Obserwowalny obiekt
+        {
+            if (sender is null)
+            {
+                throw new System.ArgumentNullException(nameof(sender));
+            }
+            if (e is null)
+            {
+                throw new System.ArgumentNullException(nameof(e));
+            }
+        };///Wydarzenie odpala się, kiedy właściwość potomka się zmienia
+        protected void OnPropertyChanged([CallerMemberName] string name = null)///Wywołuje wydarzenie PropertyChanged
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
